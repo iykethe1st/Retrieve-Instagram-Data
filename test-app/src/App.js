9426cc96b2bb208366c506f72966e1be258e23a4
Ikenna Udemezue <iykethe1st@gmail.com>
2023-02-06 08:23:11 +0100

    hot fix

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -8,13 +8,10 @@ function App() {
   const app_id = "1484600968698750";
   const redirectUri = "https://retrieve-instagram-data.vercel.app/";
 
-  // window.location.href = `https://api.instagram.com/oauth/authorize/?client_id=${app_id}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`;
-
   const getAccessToken = () => {
-    if (window.location.hash) {
-      token = window.location.href.split("=")[1];
-      setAccessToken(token);
-    }
+    window.location.href = `https://api.instagram.com/oauth/authorize/?client_id=${app_id}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`;
+    token = window.location.href.split("=")[1];
+    setAccessToken(token);
   };
   console.log(accessToken);
 
@@ -24,17 +21,12 @@ function App() {
         {accessToken ? (
           <p>Access Token: {accessToken} </p>
         ) : (
-          <a
-            href={`https://api.instagram.com/oauth/authorize/?client_id=${app_id}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`}
-            target="_blank"
+          <button
+            className="mt-24 rounded-sm ring-1 p-2"
+            onClick={getAccessToken}
           >
-            <button
-              className="mt-24 rounded-sm ring-1 p-2"
-              onClick={getAccessToken}
-            >
-              Authorize
-            </button>
-          </a>
+            Authorize
+          </button>
         )}
       </div>
     </div>
