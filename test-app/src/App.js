6d9976793d02809011bd7301a22e61a8ad4eb0df
Ikenna Udemezue <iykethe1st@gmail.com>
2023-02-06 08:09:13 +0100

    Initial commit

---
+++ test-app/src/App.js
@@ -0,0 +1,39 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [accessToken, setAccessToken] = useState("");
+  const token = "";
+  const app_id = "1484600968698750";
+  const redirectUri = "https://iykethe1st.vercel.app/";
+
+  // window.location.href = `https://api.instagram.com/oauth/authorize/?client_id=${app_id}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`;
+
+  const getAccessToken = () => {
+    if (window.location.hash) {
+      token = window.location.href.split("=")[1];
+      setAccessToken(token);
+    }
+  };
+  console.log(token);
+
+  return (
+    <div className="App">
+      <div>
+        {accessToken ? (
+          <p>Access Token: {accessToken} </p>
+        ) : (
+          <a
+            href={`https://api.instagram.com/oauth/authorize/?client_id=${app_id}&redirect_uri=${redirectUri}&scope=user_profile,user_media&response_type=code`}
+            target="_blank"
+          >
+            <button onClick={getAccessToken}>Authorize</button>
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
