93b5ee96fc0b5700da47d546a31789422b62238a
Ikenna Udemezue <iykethe1st@gmail.com>
2023-02-07 14:10:48 +0100

    Display user data

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,11 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Home from "./Home";
+import Home, { getData } from "./Home";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const key = process.env.REACT_APP_INSTAGRAM_KEY;
+      const url = `https://graph.instagram.com/me/media?fields=id,username,caption,media_url,timestamp,media_type,permalink&access_token=${key}`;
+      const data = await fetch(url);
+      const feed = await data.json();
+      setData(feed);
+    };
+    getData();
+  }, []);
   return (
     <div className="App">
-      <Home />
+      <Home feed={data} />
     </div>
   );
 }
--- test-app/src/Home.jsx
+++ test-app/src/Home.jsx
@@ -1,84 +1,17 @@
-import React, { useState, useEffect } from "react";
-
-const Home = () => {
-  const [authorizationCode, setAuthorizationCode] = useState("");
-  const [accessToken, setAccessToken] = useState("");
-  const CLIENT_ID = "1484600968698750";
-  const CLIENT_SECRET = "0035ceec7ed2c38f1af236615b75d05e";
-  const REDIRECT_URI = "https://retrieve-instagram-data.vercel.app/";
-
-  useEffect(() => {
-    const handleHashChange = () => {
-      if (window.location.hash) {
-        const token = window.location.hash.split("=")[1];
-        setAccessToken(token);
-      }
-    };
-
-    window.addEventListener("hashchange", handleHashChange);
-
-    return () => {
-      window.removeEventListener("hashchange", handleHashChange);
-    };
-  }, []);
-
-  const handleClick = () => {
-    const url = `https://api.instagram.com/oauth/authorize/?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
-    window.location.replace(url);
-  };
-
-  const query = new URLSearchParams(window.location.search);
-  const code = query.get("code");
-  if (code) {
-    setAuthorizationCode(code);
-    // retrieveAccessToken(code);
-  }
-
-  // if (window.location.hash) {
-  //   const code = window.location.href.split("=")[1];
-  //   setAuthorizationCode(code);
-  //   // retrieveAccessToken(code);
-  // }
-
-  const retrieveAccessToken = async (code) => {
-    try {
-      const response = await fetch(
-        "https://api.instagram.com/oauth/access_token",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: `client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=${REDIRECT_URI}&code=${code}`,
-        }
-      );
-      const data = await response.json();
-      setAccessToken(data.access_token);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  // console.log(accessToken);
-  // console.log("-------------------");
-  console.log(authorizationCode);
+export default function Home({ feed }) {
+  const posts = feed.data;
 
   return (
     <div>
-      <button onClick={() => handleClick()}>
-        Retrieve Authorization Code and Access Token
-      </button>
-      {authorizationCode ? (
-        <div>
-          Authorization Code: <pre>{authorizationCode}</pre>
-        </div>
-      ) : null}
-      {/* {accessToken ? (
-        <div>
-          Access Token: <span>{accessToken}</span>
-        </div>
-      ) : null} */}
+      {posts &&
+        posts.map((post) => (
+          <div key={post.id}>
+            <div>{post.username}</div>
+            <div>{post.caption}</div>
+            <div>{post.timestamp}</div>
+            <div>{post.permalink}</div>
+          </div>
+        ))}
     </div>
   );
-};
-
-export default Home;
+}
