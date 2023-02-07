f97567cd918967f9ec08814504d15e3225386b1c
Ikenna Udemezue <iykethe1st@gmail.com>
2023-02-07 08:17:13 +0100

    hot fix

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import GetUserData from "./helper/getUserData";
+import { redirect } from "react-router-dom";
 import { useEffect, useState } from "react";
-import DisplayData from "./helper/DisplayData";
+
 // import axios from "axios";
 // import getUserData from "./helper/getUserData";
 
 function App() {
   const [accessToken, setAccessToken] = useState(null);
+  const location = useLocation();
   const getAccessToken = () => {
     const app_id = "1484600968698750";
     const redirectUri = "https://retrieve-instagram-data.vercel.app/";
@@ -16,71 +17,20 @@ function App() {
   if (window.location.hash) {
     const token = window.location.href.split("=")[1];
     setAccessToken(token.split("#")[0]);
+    console.log(accessToken);
+    return redirect("/");
   }
 
-  console.log(accessToken);
-  //   useEffect(() => {
-  //     // Check if the access token is in the URL hash
-  //     const hash = window.location.hash
-  //       .substring(1)
-  //       .split("&")
-  //       .reduce(function (initial, item) {
-  //         if (item) {
-  //           var parts = item.split("=");
-  //           initial[parts[0]] = decodeURIComponent(parts[1]);
-  //         }
-  //         return initial;
-  //       }, {});
-  //     if (hash.access_token) {
-  //       setAccessToken(hash.access_token);
-  //     }
-  //   }, []);
-  //   useEffect(() => {
-  //     if (!accessToken) {
-  //       return;
-  //     }
-  //     // Make a request to the Instagram API to retrieve the user data
-  //     fetch(
-  //       `https://api.instagram.com/v1/users/self/?access_token=${accessToken}`
-  //     )
-  //       .then((response) => response.json())
-  //       .then((data) => setData(data.data));
-  //   }, [accessToken]);
-  //   // useEffect(() => {
-  //   //   const url = `https://api.instagram.com/v1/users/self/media/recent/?access_token=${accessToken}`;
-  //   //   fetch(url)
-  //   //     .then((response) => response.json())
-  //   //     .then((responseData) => setData(responseData.data));
-  //   //   console.log(data);
-  //   // }, []);
-  //   return (
-  //     <div className="App p-16">
-  //       {accessToken ? (
-  //         data ? (
-  //           <div>
-  //             <p>Username: {data.username}</p>
-  //             <p>Full name: {data.full_name}</p>
-  //             <p>
-  //               Profile picture:{" "}
-  //               <img src={data.profile_picture} alt="Profile picture" />
-  //             </p>
-  //           </div>
-  //         ) : (
-  //           <p>Loading user data...</p>
-  //         )
-  //       ) : (
-  //         <button className="ring-2 p-2 rounded " onClick={getAccessToken}>
-  //           Authorize with Instagram
-  //         </button>
-  //       )}
-  //     </div>
-  //   );
+  const insta_token =
+    "AQDWPcOOS2rSMB8JAoBuabgTDOKqAiBrCU9EN4uxGy02gXCe4BmgtzUNpcPgz-GH_9gabow_qHMyGvS7Da80K6aN4He-29HsSBi_SX_fseaKTjjaQvLiArvRNBlaiiMoc2lELgqpzXhrhw4oqGxiAdruiB7FuaDlZbiJaM3BXCXZFbjQgwUiY0TlaUYirtpRK7QAVL-fjkKNB4A6zMIXEAikn2HCGKrQtbQU6ZakEVCcdQ";
 
   return (
     <div className="App ">
-      <h1>Instagram Feed with Instagram API</h1>
-
-      <DisplayData />
+      <a onClick={getAccessToken} target="_blank">
+        <button className="ring-2 p-2 rounded-sm bg-slate-400 mt-24">
+          Authorize
+        </button>
+      </a>
     </div>
   );
 }
